168539450376c58639598dbfff48da29989eea5c
Maria Goenko <masha@dev.by>
2019-10-15 09:31:51 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -34,11 +34,40 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----':  '0'
+};
+
+const MORSE_MAPPING = {
+    '10': '.',
+    '11': '-'
 };
 
 function decode(expr) {
-    // write your solution here
+    let n = expr.length;
+    let result = '';
+    
+    //for each letter
+    for(let i = 0; i < n; i += 10) {
+
+        let curLetterEncoded = expr.substr(i, 10);
+        if(curLetterEncoded.startsWith('*')) {
+            result += ' ';
+            continue;
+        }
+
+        let startIndex = curLetterEncoded.indexOf('1');
+        let shortLetterEncoded = curLetterEncoded.substr(startIndex);
+        let letterMorseEncoded = '';
+
+        //for each pair of digits (10 or 11)
+        for(let j = 0; j < shortLetterEncoded.length; j += 2) {
+            let curSymbolMorseEncoded = MORSE_MAPPING[shortLetterEncoded.substr(j, 2)];
+            letterMorseEncoded += curSymbolMorseEncoded;
+        }
+        result += MORSE_TABLE[letterMorseEncoded];
+    }
+    
+    return result;
 }
 
 module.exports = {
